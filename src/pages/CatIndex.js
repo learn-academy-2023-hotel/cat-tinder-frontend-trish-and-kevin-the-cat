2faae4edc11659d392e73424318664e1b8fb2bb9
triciasykes <tlsykes98@gmail.com>
2023-12-08 11:42:08 -0800

    frontend structure completel

---
+++ src/pages/CatIndex.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const CatIndex = () => {
+  return <div>CatIndex</div>
+}
+
+export default CatIndex
---
+++ src/pages/CatNew.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+const CatNew = () => {
+  return <div>CatNew</div>
+}
+
+export default CatNew
