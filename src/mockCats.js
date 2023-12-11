b6c1cdcd28ea9379271f8403fa3bbcdb1248ccf0
triciasykes <tlsykes98@gmail.com>
2023-12-10 18:04:11 -0800

    minor update

--- src/mockCats.js
+++ src/mockCats.js
@@ -5,7 +5,7 @@ const mockCats = [
     age: 9,
     enjoys: "sleeping in his forts",
     image:
-      "https://images.unsplash.com/photo-1543852786-1cf6624b9987?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+      "https://images.unsplash.com/photo-1572897263855-ea51655f9f0b?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
   {
     id: 2,
