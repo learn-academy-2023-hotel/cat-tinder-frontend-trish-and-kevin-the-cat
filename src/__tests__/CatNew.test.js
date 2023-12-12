81a3a86a164d8777c45f9bfa0b8c04666efdd83d
triciasykes <tlsykes98@gmail.com>
2023-12-12 10:49:38 -0800

    adds create fxnality and testing examples

---
+++ src/__tests__/CatNew.test.js
@@ -0,0 +1,34 @@
+import { render, screen } from "@testing-library/react"
+import { BrowserRouter } from "react-router-dom"
+
+import CatNew from "../pages/CatNew"
+
+describe("<CatNew />", () => {
+  const renderNew = () => {
+    render(
+      <BrowserRouter>
+        <CatNew />
+      </BrowserRouter>
+    )
+  }
+  it("renders a new cat form", () => {
+    renderNew()
+    screen.logTestingPlaygroundURL()
+
+    const nameInput = screen.getByRole("textbox", {
+      name: /name/i,
+    })
+    expect(nameInput).toBeInTheDocument()
+
+    const enjoysInput = screen.getByRole("textbox", {
+      name: /enjoys/i,
+    })
+    expect(enjoysInput).toBeInTheDocument()
+  })
+
+  it("has a form with entries for name, age, enjoys, image", () => {
+    renderNew()
+    const formName = screen.getByText(/name/i)
+    expect(formName.getAttribute("For")).toEqual("name")
+  })
+})
